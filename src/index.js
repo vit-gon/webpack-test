14d745cdb4cf4fdb056e7d78ef5ba199ff3ea565
Vitalii Goncharov <vitalii.goncharov@aleado.ru>
2021-02-26 17:57:57 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log('Test');
+
+const heading = document.createElement('h1')
+heading.textContent = 'Наша сборка webpack работает'
+
+const root = document.querySelector('#root')
+root.append(heading)
